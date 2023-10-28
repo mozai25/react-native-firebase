b8690ddcb1fc08cc9b9a97332f6cccaaeb3eecf7
mozai25 <prolactin@gmail.com>
2023-10-28 14:57:43 -0300

    Add files via upload

---
+++ packages/admob/android/build/generated/source/buildConfig/debug/io/invertase/firebase/admob/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.invertase.firebase.admob;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "io.invertase.firebase.admob";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String FIREBASE_JSON_RAW = "{\"admob_android_app_id\":\"ca-app-pub-4170639198404846~3416626244\",\"admob_ios_app_id\":\"ca-app-pub-4170639198404846~3416626244\"}";
+}
